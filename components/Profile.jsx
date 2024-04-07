3e7f33594c7379a9604cc6778faa2ec76e99b734
Aman Tiwari <amananjalitiwari2007@gmail.com>
2024-04-07 17:44:54 +0530

    Client side fix

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -4,11 +4,11 @@ import PromptCard from '@components/PromptCard';
 import { useEffect, useState } from 'react';
 
 const Profile = ({ name, desc, posts, handleDel, handleEdit }) => {
-  const [localdata, setLocalData] = useState([]);
+  const [localData, setLocalData] = useState([]);
 
   useEffect(() => {
-    if (typeof window !== undefined) {
-      const storedData = JSON.parse(localStorage.getItem('postData')) || [];
+    if ((typeof window !== undefined) && (localStorage.getItem('postData'))) {
+      const storedData = JSON.parse(localStorage.getItem('postData'));
       setLocalData(storedData);
     }
   }, []);
@@ -21,7 +21,7 @@ const Profile = ({ name, desc, posts, handleDel, handleEdit }) => {
       </h1>
       <p className='desc'>{desc}</p>
       <div className='flex flex-wrap gap-5 my-8'>
-        {(posts.length > 0 ? posts : localdata).map((post) => {
+        {(posts.length > 0 ? posts : localData).map((post) => {
           return (
             <PromptCard
               handleDel={handleDel}
